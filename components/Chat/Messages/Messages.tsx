ea25d1a3106de4b5b486ddde346e43bd9ab9ce2c
d.farina <d.farina@manao-team.com>
2022-01-18 18:04:00 +0300

    add chat scrolling

--- components/Chat/Messages/Messages.tsx
+++ components/Chat/Messages/Messages.tsx
@@ -1,6 +1,7 @@
 import { IChatMessage } from "../../../store/types";
 import AddMessage from "../AddMessage";
 import classes from './Messages.module.sass';
+import { useEffect, useRef } from 'react';
 
 interface MessagesProps {
     messages: IChatMessage[]
@@ -8,21 +9,37 @@ interface MessagesProps {
 
 const Messages = ({ messages }: MessagesProps) => {
 
+    const listRef = useRef<HTMLDivElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!listRef.current || !containerRef.current) return;
+
+        const height = containerRef.current.offsetHeight - listRef.current.offsetHeight;
+
+        console.log(height);
+
+        containerRef.current.style.top = `-${height}px`;
+
+    }, [messages])
+
     return (
-        <div className={classes.messages}>
+        <div className={classes.messages} >
 
-            <div className={classes.messages__list}>
-                {messages && messages.map((message) => (
-                    <div key={`message-${message.user.userName}-${message.id}`} className={classes.messages__message}>
-                        <div className={classes.messages__message__header}>
-                            {message.user.userName}
-                        </div>
+            <div className={classes.messages__list} ref={listRef}>
+                <div className={classes.messages__container} ref={containerRef}>
+                    {messages && messages.map((message) => (
+                        <div key={`message-${message.user.userName}-${message.id}`} className={classes.messages__message}>
+                            <div className={classes.messages__message__header}>
+                                {message.user.userName}
+                            </div>
 
-                        <div className={classes.messages__message__body}>
-                            {message.message}
+                            <div className={classes.messages__message__body}>
+                                {message.message}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
 
             <AddMessage />
