0136d72555718fe5004902be354b4bb2253265e6
Manao-dev <pm@manao.by>
2022-01-22 21:48:13 +0300

    fix user login and logout

--- api/service.ts
+++ api/service.ts
@@ -22,21 +22,22 @@ const connect = async (user: IUser) => {
     return result.data
 };
 
-const logUserOut = async (user: IUser) => {
-    const result = await API.post("/api/logout", user);
+const disconnect = async (user: IUser) => {
+    const result = await API.post("/api/disconnect", user);
     return result.data
 };
 
 const updateOnlineUsers = async () => {
+    console.log("update online users");
     const result = await API.get("/api/users-online");
-    return result.data()
+    return result.data;
 }
 
 const service = {
     userReady,
     userUnready,
     connect,
-    logUserOut,
+    disconnect,
     updateOnlineUsers
 };
 
